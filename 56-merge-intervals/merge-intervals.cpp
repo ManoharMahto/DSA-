492496cd1f79c545c0dc2f8332bc14a9cba06aac
Manohar Mahto <manoharmahto75418@gmail.com>
2026-08-29 21:39:11 +0530

    Time: 3 ms (89.02%) | Memory: 23.6 MB (97.20%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if (intervals.empty()) return {};
-
-        // 1. Sort intervals by start time
+        int n=intervals.size();
         sort(intervals.begin(), intervals.end());
-
         vector<vector<int>> ans;
-        ans.push_back(intervals[0]);
-
-        for (int i = 1; i < intervals.size(); i++) {
-            // Check if current interval overlaps with the last merged interval
-            if (intervals[i][0] <= ans.back()[1]) {
-                // Extend the end boundary to the maximum end seen so far
-                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
-            } else {
-                // No overlap, start a new merged interval
+        for (int i = 0; i < n; i++) {
+            if(ans.empty() || intervals[i][0]>ans.back()[1]){
                 ans.push_back(intervals[i]);
+            }else{
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
             }
         }
 
